d5df70b252da1f9d5224a2905b6a934d5071efcb
gianpersia <g.persia@alumno.um.edu.ar>
2021-11-03 09:37:53 -0300

    update

---
+++ buscaminas.py
@@ -0,0 +1,12 @@
+class Buscaminas():
+    def _init_(self, rows, cols, bombs):
+        self.board = [["" for j in cols] for i in rows]
+        def question(self, movs):
+            mov = input("¿Qué movimiento desea realizar?" + movs)
+            row = input("Ingrese fila: " + movs)
+            col = input("Ingrese columna: " + movs)
+            return mov, row, col
+        def play(self, mov, row, col):
+        def show_board(self):
+        def win(self):
+        def lose(self):
